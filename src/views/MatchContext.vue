29bd6e61966215e0251e99735e9988d873d9e13e
maniu <maniu@qq>
2024-05-15 13:04:39 +0800

    Initial commit

---
+++ src/views/MatchContext.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="layout-demo">
+    <a-layout style="height: 400px">
+      <a-layout-header>比赛题目</a-layout-header>
+      <a-layout-content>
+        比赛详细内容
+        <a-col :flex="1">
+          <a-countdown
+            title="Milliseconds"
+            :value="now + 1000 * 60 * 60 * 2"
+            :now="now"
+            format="HH:mm:ss.SSS"
+          />
+        </a-col>
+      </a-layout-content>
+      <a-layout-footer>
+        <a-button type="primary" v-if="!baomingRef.value" @click="baomingClick">
+          报名
+        </a-button>
+        <a-button type="primary" v-if="baomingRef.value" disabled>
+          已报名
+        </a-button>
+        <a-button type="primary" @click="toQuestionListPage()">
+          查看题目列表
+        </a-button>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref, withDefaults } from "vue";
+import message from "@arco-design/web-vue/es/message";
+import { useRouter } from "vue-router";
+interface Props {
+  id: string;
+}
+const router = useRouter();
+const props = withDefaults(defineProps<Props>(), {
+  id: () => "",
+});
+console.log(props.id);
+const now = Date.now();
+const start = ref(true);
+
+const handleFinish = () => {
+  message.info("Finish");
+};
+const baomingRef = ref(true);
+const baomingClick = () => {
+  console.log("1111");
+  baomingRef.value = !baomingRef.value;
+  console.log(baomingRef.value);
+};
+
+const toQuestionListPage = () => {
+  router.push({
+    path: `/questionList/${props.id}`,
+  });
+};
+</script>
+<style scoped>
+.layout-demo :deep(.arco-layout-header),
+.layout-demo :deep(.arco-layout-footer),
+.layout-demo :deep(.arco-layout-sider-children),
+.layout-demo :deep(.arco-layout-content) {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: var(--color-white);
+  font-size: 16px;
+  font-stretch: condensed;
+  text-align: center;
+}
+
+.layout-demo :deep(.arco-layout-header),
+.layout-demo :deep(.arco-layout-footer) {
+  height: 64px;
+  background-color: #444444;
+}
+
+.layout-demo :deep(.arco-layout-sider) {
+  width: 206px;
+  background-color: #42b983;
+}
+
+.layout-demo :deep(.arco-layout-content) {
+  background-color: rgb(var(--arcoblue-6));
+}
+</style>
---
+++ src/views/question/RankView.vue
@@ -0,0 +1,39 @@
+<template>
+  <a-card title="排行榜" style="width: 100%">
+    <a-table :columns="columns" :data="data" pagination="false" />
+  </a-card>
+</template>
+
+<script setup>
+import { onMounted, ref, watchEffect } from "vue";
+
+const columns = [
+  {
+    title: "排名",
+    dataIndex: "rank",
+    key: "rank",
+  },
+  {
+    title: "用户名",
+    dataIndex: "username",
+    key: "username",
+  },
+  {
+    title: "得分",
+    dataIndex: "score",
+    key: "score",
+  },
+];
+
+const data = ref([
+  { rank: 1, username: "user1", score: 100 },
+  { rank: 2, username: "user2", score: 95 },
+  { rank: 3, username: "user3", score: 90 },
+]);
+</script>
+
+<style scoped>
+a-card {
+  margin: 20px;
+}
+</style>
